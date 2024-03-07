5da507f7d908df51d16b9f0e15cba86cc6b21a02
Ahmad Safii <sakmadboyz@gmail.com>
2024-03-07 21:46:50 +0700

    Adding Build Tools WorkBox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  CACHE_NAME:'Restofine-V1'
 }
 
 export default CONFIG
